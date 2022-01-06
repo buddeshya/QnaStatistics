6dacba527cc7bacb7b910d6ba1ee55db8137a74e
Uddeshya Barnwal <uddeshya.barnwal@valuence.inc>
2022-01-06 15:50:16 +0530

    * .gitignore: QnaStatistics API
    * QnaStatisticsHTTPtrigger.sln:
    * host.json:
    * .gitignore:
    * local.settings.json:
    * AzureStorageHelper.cs:
    * QnaStatisticsHTTPtrigger.cs:
    * QnaStatisticsHTTPtrigger.csproj:
    * QnaStatisticsHTTPtrigger.csproj.AssemblyReference.cache:

---
+++ QnaStatisticsHTTPtrigger/AzureStorageHelper.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QnaStatisticsHTTPtrigger
+{
+    /// <summary>
+    /// Contains Azure table storage utility methods.
+    /// </summary>
+    public static class AzureStorageHelper
+    {
+        /// <summary>
+        /// Tries to resolve a table in the storage defined by the given connection string and table name.
+        /// </summary>
+        /// <param name="connectionString">The Azure storage connection string.</param>
+        /// <param name="tableName">The name of the table to resolve and return.</param>
+        /// <returns>The resolved table or null in case of an error.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="FormatException"></exception>
+        public static CloudTable GetTable(string connectionString, string tableName)
+        {
+            CloudStorageAccount cloudStorageAccount = null;
+
+            try
+            {
+                cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
+            }
+            catch
+            {
+                throw;
+            }
+
+            CloudTableClient cloudTableClient = cloudStorageAccount?.CreateCloudTableClient();
+            return cloudTableClient?.GetTableReference(tableName);
+        }
+
+        
+    }
+}
---
+++ QnaStatisticsHTTPtrigger/QnaStatisticsHTTPtrigger.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Cosmos.Table;
+using Azure.Data.Tables;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace QnaStatisticsHTTPtrigger
+{
+
+
+    public static class QnaStatisticsHTTPtrigger
+    {
+
+        public static CloudTable GetTable(string connectionString, string tableName)
+        {
+            CloudStorageAccount cloudStorageAccount = null;
+
+            try
+            {
+                cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
+            }
+            catch
+            {
+                throw;
+            }
+
+            CloudTableClient cloudTableClient = cloudStorageAccount?.CreateCloudTableClient();
+            return cloudTableClient?.GetTableReference(tableName);
+        }
+
+
+        public static async Task<IList<T>> ExecuteTableQueryAsync<T>(
+            this CloudTable cloudTable, TableQuery<T> tableQuery,
+            CancellationToken cancellationToken = default(CancellationToken),
+            Action<IList<T>> onProgress = null) where T : ITableEntity, new()
+        {
+            var items = new List<T>();
+            TableContinuationToken tableContinuationToken = null;
+
+            do
+            {
+                TableQuerySegment<T> tableQuerySegment = null;
+
+                try
+                {
+                    tableQuerySegment = await cloudTable.ExecuteQuerySegmentedAsync<T>(tableQuery, tableContinuationToken);
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.WriteLine($"Failed to execute a table query: {e.Message}");
+                    return items;
+                }
+
+                tableContinuationToken = tableQuerySegment.ContinuationToken;
+                items.AddRange(tableQuerySegment);
+                onProgress?.Invoke(items);
+            }
+            while (tableContinuationToken != null && !cancellationToken.IsCancellationRequested);
+
+            return items;
+        }
+
+
+
+        public class RoutingDataEntity
+        {
+            public string Body { get; set; }
+        }
+
+
+
+        private static Task<IList<RoutingDataEntity>> GetAllEntitiesFromTable(CloudTable table)
+        {
+            var query = new TableQuery<RoutingDataEntity>()
+                .Where(TableQuery.GenerateFilterCondition(
+                    "PartitionKey", QueryComparisons.Equal, "question"));
+
+            return  table.ExecuteTableQueryAsync(query);
+        }
+
+
+
+
+
+        [FunctionName("QnaStatisticsHTTPtrigger")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+
+            public static CloudTable _qnaStatisticsTable;
+            public const string TableNameQnaStaistics = "QnAStatistics";
+            _qnaStatisticsTable = GetTable(connectionString, TableNameQnaStaistics);
+
+
+                var entities = GetAllEntitiesFromTable(_qnaStatisticsTable).Result;
+                var qnaStatistics = new List<QnaStatistics>();
+
+                foreach (RoutingDataEntity entity in entities)
+                {
+                    var qnaStatistics =
+                        JsonConvert.DeserializeObject<QnaStatistics>(entity.Body);
+                    qnaStatistics.Add(qnaStatistics);
+                }
+       
+
+
+            var json = JsonConvert.SerializeObject(qnaStatistics);
+            return new OkObjectResult(json);
+ 
+        }
+
+    }
+}
